0139de0000e56c92ddaf26814665e0bbeae2fa54
Oliver <oschi1303@gmail.com>
2025-05-15 20:59:26 +0200

    fancydialogs: Show welcome dialog to new players on join

--- plugins/fancydialogs/src/main/java/com/fancyinnovations/fancydialogs/listener/PlayerJoinListener.java
+++ plugins/fancydialogs/src/main/java/com/fancyinnovations/fancydialogs/listener/PlayerJoinListener.java
@@ -1,5 +1,7 @@
 package com.fancyinnovations.fancydialogs.listener;
 
+import com.fancyinnovations.fancydialogs.FancyDialogsPlugin;
+import com.fancyinnovations.fancydialogs.api.Dialog;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -8,7 +10,16 @@ public class PlayerJoinListener implements Listener {
 
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
-
+        boolean isNewPlayer = !event.getPlayer().hasPlayedBefore();
+        if (isNewPlayer) {
+            String welcomeDialogID = FancyDialogsPlugin.get().getFancyDialogsConfig().getWelcomeDialogID();
+            Dialog dialog = FancyDialogsPlugin.get().getDialogRegistry().get(welcomeDialogID);
+            if (dialog != null) {
+                dialog.open(event.getPlayer());
+            } else {
+                FancyDialogsPlugin.get().getLogger().warning("Welcome dialog with ID " + welcomeDialogID + " not found.");
+            }
+        }
     }
 
 }
