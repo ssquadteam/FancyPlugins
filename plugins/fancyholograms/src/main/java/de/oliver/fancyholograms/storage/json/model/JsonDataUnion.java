fbaac99c79a8e1c076836cda9bcdc18de8e2aa2f
Oliver <oschi1303@gmail.com>
2025-05-17 17:19:07 +0200

    fancyholograms-v3: Rename json type-specific fields

--- plugins/fancyholograms/src/main/java/de/oliver/fancyholograms/storage/json/model/JsonDataUnion.java
+++ plugins/fancyholograms/src/main/java/de/oliver/fancyholograms/storage/json/model/JsonDataUnion.java
@@ -2,10 +2,10 @@ package de.oliver.fancyholograms.storage.json.model;
 
 public record JsonDataUnion(
         JsonHologramData hologram_data,
-        JsonDisplayHologramData display_hologram_data,
+        JsonDisplayHologramData display_data,
 
-        JsonTextHologramData text_hologram_data,
-        JsonItemHologramData item_hologram_data,
-        JsonBlockHologramData block_hologram_data
+        JsonTextHologramData text_data,
+        JsonItemHologramData item_data,
+        JsonBlockHologramData block_data
 ) {
 }
