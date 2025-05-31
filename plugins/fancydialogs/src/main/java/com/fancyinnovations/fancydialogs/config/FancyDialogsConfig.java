8d3defa8202c30ccd9ea6cf467025e8ac054d12d
Oliver <oschi1303@gmail.com>
2025-05-31 10:46:52 +0200

    fancydialogs: Add quick_actions_dialog_id config option

--- plugins/fancydialogs/src/main/java/com/fancyinnovations/fancydialogs/config/FancyDialogsConfig.java
+++ plugins/fancydialogs/src/main/java/com/fancyinnovations/fancydialogs/config/FancyDialogsConfig.java
@@ -10,6 +10,7 @@ public class FancyDialogsConfig {
 
     private String language;
     private String welcomeDialogID;
+    private String quickActionsDialogID;
 
     public void load() {
         FancyDialogsPlugin.get().reloadConfig();
@@ -20,13 +21,20 @@ public class FancyDialogsConfig {
 
         welcomeDialogID = (String) ConfigHelper.getOrDefault(config, "welcome_dialog_id", "welcome-dialog");
         config.setInlineComments("welcome_dialog_id", List.of("The ID of the dialog which will be shown to the player when they join the server for the first time."));
+
+        quickActionsDialogID = (String) ConfigHelper.getOrDefault(config, "quick_actions_dialog_id", "quick-actions-dialog");
+        config.setInlineComments("quick_actions_dialog_id", List.of("The ID of the dialog which will be shown to the player when they click on the quick actions key ('G' by default)."));
+    }
+
+    public String getLanguage() {
+        return language;
     }
 
     public String getWelcomeDialogID() {
         return welcomeDialogID;
     }
 
-    public String getLanguage() {
-        return language;
+    public String getQuickActionsDialogID() {
+        return quickActionsDialogID;
     }
 }
