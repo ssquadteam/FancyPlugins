823d66a4d19b3880e9733547ef7678ef3e8d3ddb
Oliver <oschi1303@gmail.com>
2025-05-31 09:37:19 +0200

    Update paperweight and gradle wrapper

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("com.gradleup.shadow") version "9.0.0-beta11" apply false
-    id("io.papermc.paperweight.userdev") version "2.0.0-beta.16" apply false
+    id("io.papermc.paperweight.userdev") version "2.0.0-beta.17" apply false
     id("xyz.jpenilla.run-paper") version "2.3.1" apply false
     id("net.minecrell.plugin-yml.paper") version "0.6.0" apply false
     id("io.papermc.hangar-publish-plugin") version "0.1.3" apply false
