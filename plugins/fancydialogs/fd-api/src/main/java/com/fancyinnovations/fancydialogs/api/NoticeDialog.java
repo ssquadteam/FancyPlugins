a96d2eb34cad909c567fca6826d276c32d967fe0
Oliver <oschi1303@gmail.com>
2025-06-15 22:22:49 +0200

    fancydialogs: Add notice dialog to api

---
+++ plugins/fancydialogs/fd-api/src/main/java/com/fancyinnovations/fancydialogs/api/NoticeDialog.java
@@ -0,0 +1,54 @@
+package com.fancyinnovations.fancydialogs.api;
+
+import com.fancyinnovations.fancydialogs.api.data.DialogBodyData;
+import com.fancyinnovations.fancydialogs.api.data.DialogData;
+import org.bukkit.entity.Player;
+
+import java.util.List;
+import java.util.UUID;
+
+public class NoticeDialog {
+
+    private final String title;
+    private final String text;
+
+    private final DialogData dialogData;
+
+    public NoticeDialog(String title, String text) {
+        this.title = title;
+        this.text = text;
+
+        this.dialogData = new DialogData(
+                "notice_dialog_" + UUID.randomUUID(),
+                this.title,
+                this.title,
+                true,
+                List.of(
+                        new DialogBodyData(this.text)
+                ),
+                List.of()
+        );
+    }
+
+    public NoticeDialog(String text) {
+        this("Notice", text);
+    }
+
+    public static void show(Player player, String text) {
+        new NoticeDialog(text).show(player);
+    }
+
+    public void show(Player player) {
+        FancyDialogs.get()
+                .createDialog(dialogData)
+                .open(player);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getText() {
+        return text;
+    }
+}
