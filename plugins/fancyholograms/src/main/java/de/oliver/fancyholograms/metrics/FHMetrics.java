2f15f203091bcc11bfad70a5bdb5c508b240519b
Oliver <oschi1303@gmail.com>
2025-06-27 16:04:12 +0200

    fancyholograms-v3: Update to new version config

--- plugins/fancyholograms/src/main/java/de/oliver/fancyholograms/metrics/FHMetrics.java
+++ plugins/fancyholograms/src/main/java/de/oliver/fancyholograms/metrics/FHMetrics.java
@@ -13,15 +13,12 @@ public class FHMetrics {
 
     private ExtendedFancyLogger logger;
     private FancyAnalyticsAPI fancyAnalytics;
-    private boolean isDevelopmentBuild = true;
 
     public FHMetrics() {
         logger = FancyHologramsPlugin.get().getFancyLogger();
     }
 
     public void register() {
-        isDevelopmentBuild = !FancyHologramsPlugin.get().getVersionConfig().getBuild().equalsIgnoreCase("undefined");
-
         fancyAnalytics = new FancyAnalyticsAPI("3b77bd59-2b01-46f2-b3aa-a9584401797f", "E2gW5zc2ZTk1OGFkNGY2ZDQ0ODlM6San");
         fancyAnalytics.getConfig().setDisableLogging(true);
 
@@ -59,7 +56,7 @@ public class FHMetrics {
         fancyAnalytics.registerNumberMetric(new MetricSupplier<>("amount_holograms", () -> (double) registry.getAll().size()));
         fancyAnalytics.registerStringMetric(new MetricSupplier<>("enabled_update_notifications", () -> FancyHologramsPlugin.get().getFHConfiguration().areVersionNotificationsMuted() ? "false" : "true"));
         fancyAnalytics.registerStringMetric(new MetricSupplier<>("fflag_disable_holograms_for_bedrock_players", () -> FHFeatureFlags.DISABLE_HOLOGRAMS_FOR_BEDROCK_PLAYERS.isEnabled() ? "true" : "false"));
-        fancyAnalytics.registerStringMetric(new MetricSupplier<>("using_development_build", () -> isDevelopmentBuild ? "true" : "false"));
+        fancyAnalytics.registerStringMetric(new MetricSupplier<>("using_development_build", () -> FancyHologramsPlugin.get().getVersionConfig().isDevelopmentBuild() ? "true" : "false"));
 
         fancyAnalytics.registerStringArrayMetric(new MetricSupplier<>("hologram_type", () -> {
             if (registry == null) {
@@ -79,7 +76,7 @@ public class FHMetrics {
         Metrics metrics = new Metrics(FancyHologramsPlugin.get(), 17990);
         metrics.addCustomChart(new Metrics.SingleLineChart("total_holograms", () -> FancyHologramsPlugin.get().getRegistry().getAll().size()));
         metrics.addCustomChart(new Metrics.SimplePie("update_notifications", () -> FancyHologramsPlugin.get().getFHConfiguration().areVersionNotificationsMuted() ? "No" : "Yes"));
-        metrics.addCustomChart(new Metrics.SimplePie("using_development_build", () -> isDevelopmentBuild ? "Yes" : "No"));
+        metrics.addCustomChart(new Metrics.SimplePie("using_development_build", () -> FancyHologramsPlugin.get().getVersionConfig().isDevelopmentBuild() ? "Yes" : "No"));
     }
 
     public FancyAnalyticsAPI getFancyAnalytics() {
