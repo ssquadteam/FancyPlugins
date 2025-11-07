d36af3cd003e8bc06f653a505c72136fba597554
Oliver <oschi1303@gmail.com>
2025-11-07 14:09:42 +0100

    Update gradle plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
-    id("com.gradleup.shadow") version "9.0.0-beta17" apply false
-    id("io.papermc.paperweight.userdev") version "2.0.0-beta.17" apply false
-    id("xyz.jpenilla.run-paper") version "2.3.1" apply false
-    id("de.eldoria.plugin-yml.paper") version "0.7.1" apply false
+    id("com.gradleup.shadow") version "9.2.2" apply false
+    id("io.papermc.paperweight.userdev") version "2.0.0-beta.19" apply false
+    id("xyz.jpenilla.run-paper") version "3.0.2" apply false
+    id("de.eldoria.plugin-yml.paper") version "0.8.0" apply false
 }
 
 allprojects {
