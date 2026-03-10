074e3c9df8626c3c5a3f42127fe2d37b466f393f
Oliver <oschi1303@gmail.com>
2026-03-10 15:54:02 +0100

    fancyworlds: Add static method to retrieve FancyWorldsConfig instance

--- plugins/fancyworlds/fw-api/src/main/java/com/fancyinnovations/fancyworlds/api/FancyWorldsConfig.java
+++ plugins/fancyworlds/fw-api/src/main/java/com/fancyinnovations/fancyworlds/api/FancyWorldsConfig.java
@@ -2,6 +2,10 @@ package com.fancyinnovations.fancyworlds.api;
 
 public interface FancyWorldsConfig {
 
+    static FancyWorldsConfig get() {
+        return FancyWorlds.get().getFancyWorldsConfig();
+    }
+
     String getLogLevel();
 
     boolean areVersionNotificationsMuted();
