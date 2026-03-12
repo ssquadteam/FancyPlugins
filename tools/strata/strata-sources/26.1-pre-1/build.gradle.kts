72487de204287b9299d46e50041cbb45c9e1cdd9
Oliver <oschi1303@gmail.com>
2026-03-12 17:23:20 +0100

    strata: First set of patches

--- tools/strata/strata-sources/26.1-pre-1/build.gradle.kts
+++ tools/strata/strata-sources/26.1-pre-1/build.gradle.kts
@@ -42,6 +42,7 @@ tasks {
         // Set the release flag. This configures what version bytecode the compiler will emit, as well as what JDK APIs are usable.
         // See https://openjdk.java.net/jeps/247 for more information.
         options.release.set(25)
+        options.compilerArgs.addAll(listOf("-Xmaxerrs", "10000"))
     }
 
     java {
